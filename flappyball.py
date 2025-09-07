98dfa35cc7625f4d5c5df2e9fe8a403decac8f91
moyo-1234 <folake.abudiore@gmail.com>
2025-09-07 19:04:36 +0100

    Add files via upload

--- flappyball.py
+++ flappyball.py
@@ -11,9 +11,38 @@ class ball():
         self.radius = radius
     def draw(self):
         screen.draw.filled_circle((self.x,self.y),self.radius,self.color)
-bal = ball(250,250,20,"purple")  
+bal = ball(250,250,20,"purple")
+bal1 = ball(375,450,30,"pink") 
+bal2 = ball(100,137,10,"blue")  
 def draw():
     bal.draw()
+    bal1.draw()
+    bal2.draw()
 
-pgzrun.go()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
           
+pgzrun.go()
